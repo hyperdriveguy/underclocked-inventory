bf12ce29472c9ea9f5e70f65e1b866f379e26e39
Carson <hyperdriveguy@gmail.com>
2022-08-03 09:48:52 -0700

    QR generation works, sends to print queue Unable to test print results at this time

--- src/make_qr.py
+++ src/make_qr.py
@@ -2,7 +2,21 @@ import qrcode
 import subprocess
 
 def print_barcode(data):
+    """Prints a QR code for any given data.
+
+    Args:
+        data: data to be encoded on the QR code
+
+    Returns:
+        tuple( printer_message, printer_errors, exit_code )
+    """
     pil_qr = qrcode.make(data=data, error_correction=qrcode.constants.ERROR_CORRECT_Q)
+    # TODO don't hardcode some directories?
     pil_qr.save('/tmp/inventory_qr.png')
-    subprocess.run('lp -d Reciept /tmp/inventory_qr.png', check=True)
-    
+    print_status = subprocess.run(['lp', '-d', 'Reciept', '/tmp/inventory_qr.png'], text=True, capture_output=True)
+    # print('lp says:', print_status.stdout.strip())
+    # print('errors:', print_status.stderr.strip())
+    # print('exit code: ', print_status.returncode)
+    return (print_status.stdout.strip(),
+        print_status.stderr.strip(),
+        print_status.returncode)
